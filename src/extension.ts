3768e1573ca3f7700e073e7ae24b8d221415d41d
misterJS <64055552+misterJS@users.noreply.github.com>
2024-07-11 16:35:59 +0700

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -10,14 +10,26 @@ export function activate(context: vscode.ExtensionContext) {
 
         const document = editor.document;
         const fullText = document.getText();
-        
-        const formattedText = fullText.replace(/>([A-Za-z\s]+)</g, (match, p1) => {
-            const suggestionCode = suggestions[p1.trim()];
-            if (suggestionCode) {
-                return ` data-jex-ml="${suggestionCode}">${p1}<`;
-            }
-            return match;
-        });
+        const fileExtension = document.fileName.split('.').pop();
+
+        let formattedText: string;
+        if (fileExtension === 'js') {
+            formattedText = fullText.replace(/"([^"]*)"/g, (match, p1) => {
+                const suggestionCode = suggestions[p1.trim()];
+                if (suggestionCode) {
+                    return `jexML.get("JS_${suggestionCode}") ?? "${p1}"`;
+                }
+                return match;
+            });
+        } else {
+            formattedText = fullText.replace(/>([A-Za-z\s]+)</g, (match, p1) => {
+                const suggestionCode = suggestions[p1.trim()];
+                if (suggestionCode) {
+                    return ` data-jex-ml="${suggestionCode}">${p1}<`;
+                }
+                return match;
+            });
+        }
 
         const fullRange = new vscode.Range(
             document.positionAt(0),
